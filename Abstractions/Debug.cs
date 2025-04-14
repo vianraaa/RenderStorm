7bb50260517cdfb133396ef245af0d0a9ee9a55c
viz <142146272+nexacopic@users.noreply.github.com>
2025-04-14 13:02:40 +0100

    going batshit crazy

---
+++ Abstractions/Debug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace RenderStorm.Abstractions;
+
+public class Debug
+{
+    internal static async void Crash(string message)
+    {
+        Error($"Crash: {message}");
+        Environment.FailFast($"rs crash: {message}");
+    }
+
+    public static void Log(dynamic message, [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
+    {
+        Console.WriteLine($"[ LOG {Path.GetFileName(file)} ( {caller} ) ] {message}");
+    }
+
+    public static void Error(dynamic message, [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[ ERR {Path.GetFileName(file)} ( {caller} ) ] {message}");
+        Console.ResetColor();
+    }
+
+    public static void Warn(dynamic message, [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"[ WRN {Path.GetFileName(file)} ( {caller} ) ] {message}");
+        Console.ResetColor();
+    }
+}
